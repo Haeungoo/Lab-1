77fc68d8b74eb413d769cbb3fbb1f9552fa6347d
구하은 <haeuni@guha-eun-ui-MacBookPro.local>
2025-05-08 19:30:16 -0400

    Update

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,7 +4,6 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-
         doFizzBuzz();
     }
 
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Reduce {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        reduceNumberTo0(number);
+    }
+
+    private static void reduceNumberTo0(int n) {
+        int step = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n -= 1;
+            }
+            step++;
+        }
+        System.out.println(step);
+    }
+
 }
